02899bf98a21d3e22b6fe03882c49efbc20586a9
nidozhu <519806169@qq.com>
2015-01-16 18:12:29 +0800

    first commit

---
+++ src/com/nidozhu/widgetdemo/ExampleAppWidgetProvider.java
@@ -0,0 +1,20 @@
+package com.nidozhu.widgetdemo;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;  
+import android.appwidget.AppWidgetProvider;  
+import android.content.Context;  
+import android.content.Intent;  
+import android.os.Bundle;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+import android.util.Log;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+
+}
